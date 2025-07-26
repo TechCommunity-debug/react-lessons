91dc125ae03fe355a6a1a0147d6b239f75ef99bb
TechCommunity-debug <techcommunity611@gmail.com>
2025-07-26 22:37:50 +0530

    Redux: 314. Attaching Payloads to Actions.

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,9 @@ const reducer = (state = { counter: 0 }, action) => {
   if (action.type === "INCREMENT") {
     return { counter: state.counter + 1 };
   }
+  if (action.type === "INCREASE") {
+    return { counter: state.counter + action.amount };
+  }
   if (action.type === "DECREMENT") {
     return { counter: state.counter - 1 };
   }
