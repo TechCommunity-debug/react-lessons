0194540b79672bb5a82ff779dfce8b7d834eca31
TechCommunity-debug <techcommunity611@gmail.com>
2025-08-16 16:14:35 +0530

    Routing: 352. Defining & Using Dynamic Routes.

--- src/pages/product.js
+++ src/pages/product.js
@@ -1,11 +1,14 @@
 function ProductsPage() {
-    return (
-      <div>
-        <h1>Welcome to the ProductsPage</h1>
-        <p>This is the ProductsPage of our application.</p>
-      </div>
-    );
-  }
-  
-  export default ProductsPage;
-  
+  return (
+    <div>
+      <h1>Welcome to the ProductsPage</h1>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ProductsPage;
