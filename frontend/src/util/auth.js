c09684876137962419c69e2feffc0680fd7f1b8f
TechCommunity-debug <techcommunity611@gmail.com>
2025-08-26 18:51:59 +0530

    test

--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,12 +1,12 @@
 import { redirect } from 'react-router-dom';
 
-export function getTokenDuration() {
-  const storedExpirationDate = localStorage.getItem('expiration');
-  const expirationDate = new Date(storedExpirationDate);
-  const now = new Date();
-  const duration = expirationDate.getTime() - now.getTime();
-  return duration;
-}
+// export function getTokenDuration() {
+//   const storedExpirationDate = localStorage.getItem('expiration');
+//   const expirationDate = new Date(storedExpirationDate);
+//   const now = new Date();
+//   const duration = expirationDate.getTime() - now.getTime();
+//   return duration;
+// }
 
 export function getAuthToken() {
   const token = localStorage.getItem('token');
@@ -15,11 +15,11 @@ export function getAuthToken() {
     return null;
   }
 
-  const tokenDuration = getTokenDuration();
+  // const tokenDuration = getTokenDuration();
 
-  if (tokenDuration < 0) {
-    return 'EXPIRED';
-  }
+  // if (tokenDuration < 0) {
+  //   return 'EXPIRED';
+  // }
 
   return token;
 }
