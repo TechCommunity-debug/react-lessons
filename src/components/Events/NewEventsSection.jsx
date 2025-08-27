9b61f9955790f353f803b2dd2d7f4dbb5268f834
TechCommunity-debug <techcommunity611@gmail.com>
2025-08-27 17:00:52 +0530

    Tanstack Query: 412. Dynamic Query Functions & Query Keys..

--- src/components/Events/NewEventsSection.jsx
+++ src/components/Events/NewEventsSection.jsx
@@ -9,8 +9,8 @@ export default function NewEventsSection() {
   const { data, isPending, isError, error } = useQuery({
     queryKey: ["events"],
     queryFn: fetchEvents,
-    // staleTime: 5000,
-    gcTime: 100,
+    staleTime: 5000,
+    // gcTime: 100,
   });
 
   let content;
