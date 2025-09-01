6b41938367736fbfa1abeea3fd7bb94427194887
TechCommunity-debug <techcommunity611@gmail.com>
2025-09-01 09:31:33 +0530

    React best practices & patterns: Compound Components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import Accordian from "./components/Accordian/Accordian";
+import AccordianItem from "./components/Accordian/AccordianItem";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <section>
+        <h2>Why work with us?</h2>
+        <Accordian className="accordian">
+          <AccordianItem className="accordian-item" title="We got 20 years of experience">
+            <article>
+              <p>You can&apos;t go wrong with us.</p>
+            <p>
+              Our team of seasoned professionals brings a wealth of knowledge and
+              experience to the table, ensuring top-notch solutions for your
+              business needs.
+            </p>
+            </article>
+          </AccordianItem>
+          <AccordianItem className="accordian-item" title="2. We got 20 years of experience">
+            <article>
+              <p>2. You can&apos;t go wrong with us.</p>
+            <p>
+              2. Our team of seasoned professionals brings a wealth of knowledge and
+              experience to the table, ensuring top-notch solutions for your
+              business needs.
+            </p>
+            </article>
+          </AccordianItem>
+        </Accordian>
+      </section>
+    </main>
+  );
 }
 
 export default App;
