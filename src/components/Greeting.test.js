8f9603974614f99a0c24c4d80e340ea831e563e1
TechCommunity-debug <techcommunity611@gmail.com>
2025-09-04 23:46:04 +0530

    Testing: 573. Testing User Interaction & State.

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 describe("Greeting Component", () => {
@@ -13,5 +14,31 @@ describe("Greeting Component", () => {
     // Assert
     expect(headingElement).toBeInTheDocument();
     expect(paragraphElement).toBeInTheDocument();
+  });
+
+  test("renders 'Changed!' text when button is clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+    const outputElement = screen.getByText("Changed!");
+
+    // Assert
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("does not render 'We are glad to have you here.' text when button is clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // ACT
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+    const outputElement = screen.queryByText("We are glad to have you here.");
+
+    // Assert
+    expect(outputElement).toBeNull();
   });
 });
